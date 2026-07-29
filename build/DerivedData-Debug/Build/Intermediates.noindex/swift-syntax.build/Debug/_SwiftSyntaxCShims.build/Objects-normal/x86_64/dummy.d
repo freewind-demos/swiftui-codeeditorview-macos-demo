c105eb9acb6d6e38e283071934d00a8faa12a442
Freewind <nowindlee@gmail.com>
2026-07-29 22:10:10 +0800

    Quick Commit

---
+++ build/DerivedData-Debug/Build/Intermediates.noindex/swift-syntax.build/Debug/_SwiftSyntaxCShims.build/Objects-normal/x86_64/dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/peng.li/workspace/freewind-demos/swiftui-codeeditorview-macos-demo/build/DerivedData-Debug/SourcePackages/checkouts/swift-syntax/Sources/_SwiftSyntaxCShims/include/module.modulemap \
+  /Users/peng.li/workspace/freewind-demos/swiftui-codeeditorview-macos-demo/build/DerivedData-Debug/SourcePackages/checkouts/swift-syntax/Sources/_SwiftSyntaxCShims/dummy.c
